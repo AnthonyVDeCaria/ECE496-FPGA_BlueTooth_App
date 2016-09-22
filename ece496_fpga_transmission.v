c0f6d083e2a5d40075b9d6e66316b888d1db9d60
LuckyChewie <chewie.decaria@mail.utoronto.ca>
2016-09-22 14:34:45 -0400

    Created a larger transmission file - not tested. Renamed serializer to make it more general.

--- ece496_fpga_transmission.v
+++ ece496_fpga_transmission.v
@@ -1,53 +1,80 @@
 /*
 Anthony De Caria - September 21, 2016
 
-This is a module to .
+This is a module to send a larger chunk of data out serially for ECE496.
+This module selects a byte of data to send from a 32 byte chain and sends it to serializer_8bit to send.
+Why 32 bytes? Cause hopefully that's all we'll need. 
 */
 
-module ece496_fpga_transmission (clock, resetn, start_transmission, finish_transmission, tx);
-	input clock, resetn, start_transmission;
-	output finish_transmission, tx;
-	
-	wire [7:0] q_out;
-	wire s_load, s_enable, s_reset;
-	wire i_load, i_enable, i_reset;
-	wire [3:0] i_out, a_out;
-	
-	parameter S0 = 2'b00, S1 = 2'b01, S2 = 2'b10, SD = 2'b11;
-	reg [1:0] curr, next;
-	
-	assign i_load = (curr == S2);
-	assign i_enable = (curr == S2);
-	assign i_reset = ~(~resetn | (curr == S0) | (curr == SD));
-	
-	assign s_load = (curr == S1);
-	assign s_enable = (curr == S1) | (curr == S2);
-	assign s_reset = ~(~resetn | (curr == S0) | (curr == SD));
+module ece496_fpga_transmission (clock, resetn, start, done, data, data_size, line_out);
+	input clock, resetn;
+	input start;
+	input [255:0] data;
+	input reg [7:0] data_size;
 
-	register_4bit_enable_async i(.clk(clock), .resetn(i_reset), .enable(i_enable), .select(i_load), .d(a_out), .q(i_out) );
-	adder_subtractor_4bit a_i(.a(i_out), .b(4'b0001), .want_subtract(1'b0), .c_out(), .s(a_out) );
+	output line_out;
 	
-	Shift_Register_8_Enable_Async_OneLoad test_reg(.clk(clock), .resetn(s_reset), .enable(s_enable), .select(s_load), .d(8'h8F), .q(q_out) );
-	
-	assign tx = q_out[7];
-	assign finish_transmission = (curr == SD);
-	
+	output done;
+	assign done = (curr == Complete);
+
+	wire [7:0] byte;
+
+	parameter Idle = 3'b000, Initialize = 3'b001, Load_Byte = 3'b010, Byte_Transmission = 3'b011, Restart_BT = 3'b011, Complete = 3'b100;
+	reg [2:0] curr, next;
+
+	wire s_t_b, f_t_b;
+	assign s_t_b = (curr == Byte_Transmission)
+
+	integer i, n; 
+	always @(*)
+	begin
+		if ((curr == Idle) | (curr == Complete))
+		begin
+			i = 0;
+			n = 0;
+		end
+		if (curr == Initialize)
+		begin
+    		n = data_size;
+		end
+		if (curr == Restart_BT)
+		begin
+			i = i + 1;
+		end
+	end
+
+	wire b_reset, b_enable, b_load;
+	assign b_reset = ~((curr == Idle) | (curr == Complete) | (curr == Restart_BT));
+	assign b_enable = (curr == Load_Byte);
+	assign b_load = (curr == Load_Byte);
+	register_8bit_enable_async r_byte(.clk(clock), .resetn(b_reset), .enable(b_enable), .select(b_load), .d(data[i * 8 +: 8]), .q(byte) );
+
+	serializer_8bit byte_sender(.clock(clock), .resetn(resetn), .data(byte) .start_transmission(s_t_b), .finish_transmission(f_t_b), .tx(line_out) );
+
 	/*
 		FSM
 	*/
 	always@(*)
 	begin
 		case(curr)
-			S0: if(start_transmission) next = S1; else next = S0;
-			S1: next = S2;
-			S2: if(!a_out[3]) next = S2; else next = SD;
-			SD: if(start_transmission) next = SD; else next = S0;
+			Idle: if(start) next = Initialize; else next = Idle;
+			Initialize: next = Load_Byte;
+			Load_Byte: next = Byte_Transmission;
+			Byte_Transmission: 
+			begin
+				if(!f_t_b) next = Initialize; 
+				if(i < n) next = Restart_BT;
+				else next = Complete;
+			end
+			Restart_BT: next = Byte_Transmission;
+			Complete: if(start) next = Complete; else next = Idle;
 		endcase
 	end
 	
 	always@(posedge clock or negedge resetn)
 	begin
-		if(!resetn) curr <= S0; else curr <= next;
+		if(!resetn) curr <= Idle; else curr <= next;
 	end
 	
 endmodule
+
